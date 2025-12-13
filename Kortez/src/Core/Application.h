82d4577949d60d4bfd9950051fd8b179d943421e
lostojicc <ostojiclukaftn@gmail.com>
2025-12-13 17:11:43 +0100

    Initial project setup

---
+++ Kortez/src/Core/Application.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
---
+++ Sandbox/src/main.cpp
@@ -0,0 +1,5 @@
+#include "Kortez/Application.h"
+
+int main() {
+    
+}
